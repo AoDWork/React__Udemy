c8d9de7d10b0d7c7ed78641f879c80f41539251f
AoDWork <88074725+AoDWork@users.noreply.github.com>
2023-06-19 15:27:49 +0300

    06 in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const Header = () => {
+  return <h2>Hello</h2>
+}
+
+const Field = () => {
+  return <input placeholder="Type here" type="text" />
+}
+
+function Btn() {
+  const text = "Log In"
+  // const res = () => {
+  //   return "Log In"
+  // }
+  // const p = <p>Log In</p>
+  const logged = true
+
+  // return  <button>{ p }</button>
+  // return  <button>{ 3+4 }</button>
+  // return  <button>{res()}</button>
+
+  return  <button>{ logged ? "Enter" : text }</button>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = "Hello";
-
-const elem = (
-  <div>
-    <h2>Text: {text}</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
