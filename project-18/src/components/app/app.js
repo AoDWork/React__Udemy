d3b17f8e7c2d697c6e388d452cbdf85f87cfc3f3
AoDWork <aodtest07@gmail.com>
2023-07-06 21:56:45 +0300

    18 in progress
    delete old package

--- project-18/src/components/app/app.js
+++ project-18/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,49 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  
-  const data = [
-    {name: "John C.", salary: 800, increase: true, id: 1},
-    {name: "Alex M.", salary: 300, increase: true, id: 2},
-    {name: "Carl W.", salary: 5000, increase: false, id: 3}
-  ]
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        { name: "John C.", salary: 800, increase: true, id: 1 },
+        { name: "Alex M.", salary: 300, increase: true, id: 2 },
+        { name: "Carl W.", salary: 5000, increase: false, id: 3 }
+      ]
+    }
+  }
 
-  return (
-    <div className="app">
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id)
+
+      const befor = data.slice(0, index)
+      const after = data.slice(index + 1)
+
+      const newArr = [ ...befor, ...after]
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
         <AppInfo />
 
         <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+          <SearchPanel />
+          <AppFilter />
         </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm />
+      </div>
+    )
+  }
+
 }
 
 export default App;
