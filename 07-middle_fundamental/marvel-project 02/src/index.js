9608dbe1b0fe342cb69487fc6ca79a987101d5fc
AoDWork <88074725+AoDWork@users.noreply.github.com>
2023-08-11 14:14:55 +0300

    07-02 done
    add base url + api key, make test request

--- 07-middle_fundamental/marvel-project 02/src/index.js
+++ 07-middle_fundamental/marvel-project 02/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelSevices';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+
+
+marvelService.getCharacter(1011052).then(res => console.log(res.data.results.name))
+
 
 ReactDOM.render(
   <React.StrictMode>
