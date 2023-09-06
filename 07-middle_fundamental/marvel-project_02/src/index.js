958b6ef18125fcee1358ed3cca873ca69a12fcf3
AoDWork <88074725+AoDWork@users.noreply.github.com>
2023-09-06 14:45:56 +0300

    03 done
    todo homework

--- 07-middle_fundamental/marvel-project_02/src/index.js
+++ 07-middle_fundamental/marvel-project_02/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelSevices';
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
-
-marvelService.getCharacter(1011052).then(res => res.data.results.forEach(item => console.log(item.name)))
 
 
 ReactDOM.render(
