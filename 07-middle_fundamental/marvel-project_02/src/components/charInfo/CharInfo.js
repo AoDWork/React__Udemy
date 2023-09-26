18ae36c3a205657c0504e659445cf3037c5abf06
AoD <aodtest07@gmail.com>
2023-09-26 11:51:34 +0300

    07-06 done
    homework done

--- 07-middle_fundamental/marvel-project_02/src/components/charInfo/CharInfo.js
+++ 07-middle_fundamental/marvel-project_02/src/components/charInfo/CharInfo.js
@@ -22,7 +22,7 @@ class CharInfo extends Component {
     }
 
     componentDidUpdate(prevProps) {
-        if(this.props.charId !== prevProps.charId){
+        if (this.props.charId !== prevProps.charId) {
             this.updateChar()
         }
     }
@@ -62,12 +62,12 @@ class CharInfo extends Component {
 
 
     render() {
-        const {char, loading, error} = this.state
+        const { char, loading, error } = this.state
 
         const skeleton = char || loading || error ? null : <Skeleton />
-        const spinner = loading  ? <Spinner /> : null
+        const spinner = loading ? <Spinner /> : null
         const errorMessage = error ? <ErrorMessage /> : null
-        const content = !( loading || error || !char) ? <View char={char} /> : null
+        const content = !(loading || error || !char) ? <View char={char} /> : null
 
 
         return (
@@ -81,61 +81,45 @@ class CharInfo extends Component {
     }
 }
 
-const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki} = char
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki, comics } = char
+    const noComics = comics.length === 0 ? <li>This character is not present in the comics</li> : null
+    const comicsList = comics.slice(0, 10)
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'unset'};
+    }
 
     return (
         <>
-        <div className="char__basics">
-        <img src={thumbnail} alt={name} />
-        <div>
-            <div className="char__info-name">{name}</div>
-            <div className="char__btns">
-                <a href={homepage} className="button button__main">
-                    <div className="inner">homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                    <div className="inner">Wiki</div>
-                </a>
+            <div className="char__basics">
+                <img src={thumbnail} style={imgStyle} alt={name} />
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div className="char__descr">
-        {description}
-    </div>
-    <div className="char__comics">Comics:</div>
-    <ul className="char__comics-list">
-        <li className="char__comics-item">
-            All-Winners Squad: Band of Heroes (2011) #3
-        </li>
-        <li className="char__comics-item">
-            Alpha Flight (1983) #50
-        </li>
-        <li className="char__comics-item">
-            Amazing Spider-Man (1999) #503
-        </li>
-        <li className="char__comics-item">
-            Amazing Spider-Man (1999) #504
-        </li>
-        <li className="char__comics-item">
-            AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-            Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-            Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-            Vengeance (2011) #4
-        </li>
-        <li className="char__comics-item">
-            Avengers (1963) #1
-        </li>
-        <li className="char__comics-item">
-            Avengers (1996) #1
-        </li>
-    </ul>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {noComics}
+                {   
+                    comicsList.map((item, i) => {
+                        return <li key={i} className="char__comics-item">
+                                    {item.name}
+                                </li>
+                    })
+                }
+            </ul>
         </>
     )
 }
